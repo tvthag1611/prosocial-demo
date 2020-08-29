dc5ba261fcaf5e19c4ed2590b4bbaf292c9ed0bb
DucNguyen1111 <62923846+DucNguyen1111@users.noreply.github.com>
2020-08-29 15:03:00 +0700

    edited by me

--- src/view/Home/Navar.js
+++ src/view/Home/Navar.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './Styles/Navar.css'
-import {Link, useRouteMatch, useLocation} from 'react-router-dom'
+import useClickOutside from './Function/useClickOutside'
+import { Link, useRouteMatch, useLocation, NavLink } from 'react-router-dom'
 export default function Navar() {
 
-  let [searchBoxContent, setSetSearchBoxContent] = useState('');
+  let [searchBoxContent, setSetSearchBoxContent] = useState(null);
   let [showMultiTask, setShowMultiTask] = useState(false);
   let [showNotification, setShowNotification] = useState(false);
   const handleClick = (e) => {
@@ -45,80 +46,93 @@ export default function Navar() {
           {item.action}
 
         </div>
-        <div className="notification-time"><small>{item.timeCreated}</small></div>
+        <div className="notification-time"><smal>{item.timeCreated}</smal></div>
       </div >
     )
   })
-
+  const refTasks = useRef()
+  const refNotis = useRef()
+  useClickOutside(refTasks, () => {
+    if (showMultiTask == true) {
+      setShowMultiTask(false)
+    }
+  })
+  useClickOutside(refNotis, () => {
+    if (showNotification == true) {
+      setShowNotification(false)
+    }
+  })
   let { pathname } = useLocation();
-
   return (
-    <>
-      {
-      pathname !== "/groups/create" ?
-      <nav className="nav">
-        <div className="row nav-bar col-sm-12">
-          <div className="nav-currency col-sm-12 col-md-6 col-lg-8 col-xl-8">
-            <img src="/img/ProPTIT-logo.png" className="nav-bar-brand" alt="proptit"/>
-            <form>
-              <input
-                type="text"
-                placeholder="Search..."
-                value={searchBoxContent}
-                onChange={handleChange}
-                id="searchAll"
-                className="nav-bar-search-input" />
-              <button
-                type="button"
-                className="btn-search bg-grey"
-                onClick={handleClick}
-              >
-                <i className="fas fa-search nav-bar-search"></i>
-              </button>
-            </form>
-          </div>
 
-          <div className="nav-currency col-sm-12 col-md-6 col-lg-4 col-xl-4">
-            <Link to="/" className={pathname === "/" ? "nav-active" : ""}><i className="fas fa-home nav-bar-icon"></i></Link>
-            <Link to="/groups" className={pathname === "/groups" ? "nav-active" : ""}><i className="fas fa-user-friends nav-bar-icon"></i></Link>
-            <a><i className="fas fa-image nav-bar-icon "></i></a>
-            <a><i className="fas fa-comments nav-bar-icon"></i></a>
-            <i
-              className="fas fa-bell nav-bar-icon noti"
-              onClick={onShowNoticationClick}
-            ><i className="fas fa-circle noti-item"></i></i>
+    <nav className="nav ">
+      <div className="row nav-bar col-sm-12">
+        <div className="nav-currency col-sm-12 col-md-6 col-lg-8 col-xl-8">
+          <img src="img/ProPTIT-logo.png" className="nav-bar-brand" />
+          <form >
+            <input
+              type="text"
+              placeholder="Search..."
+              value={searchBoxContent}
+              onChange={handleChange}
+              className="nav-bar-search-input" />
+            <button
+              type="button"
+              className="btn-search"
+              onClick={handleClick}
+            >
+              <i class="fas fa-search nav-bar-search bg-grey"></i>
+            </button>
+          </form>
+        </div>
+
+        <div className="nav-currency col-sm-12 col-md-6 col-lg-4 col-xl-4">
+          <NavLink exact={true} activeClassName='nav-active' to="/" ><i className="fas fa-home nav-bar-icon"></i></NavLink>
+          <NavLink activeClassName='nav-active' to="/groups" ><i className="fas fa-user-friends nav-bar-icon"></i></NavLink>
+          <a><i class="fas fa-image nav-bar-icon "></i></a>
+          <a><i class="fas fa-comments nav-bar-icon"></i></a>
+          <i
+            class="fas fa-bell nav-bar-icon noti"
+            onClick={onShowNoticationClick}
+          >
+            <span className={showNotification ? "rectangle " : "rectangle dp-none"}></span>
+            <i class="fas fa-circle noti-item"></i></i>
+          <i
+            class="fas nav-img  nav-bar-icon "
+            onClick={onShowMultiTaskClick}
+          >
             <img
               className="user-ava"
               onClick={onShowMultiTaskClick}
               src="https://sohanews.sohacdn.com/2020/2/26/photo-1-158270587240769675748.jpg"
               alt="avatar-user"
             />
-          </div>
-
-          <div className={showMultiTask ? "dropdown-profile " : "dropdown-profile dp-none"}>
-            <div className="block-container">
-                <div className="dropdown-item">
-                  <Link
-                    to="/profile" className={pathname === "/" ? "nav-active" : ""}
-                  >
-                    <i className="far fa-user-circle"></i>
-                    Profile
-                  </Link>
-                </div>
-              <div className="dropdown-item"><a><i className="fas fa-cog"></i>Setting</a></div>
-              <div className="dropdown-item"><a><i className="far fa-envelope"></i>Inbox</a></div>
-              <div className="dropdown-item"><a><i className="far fa-question-circle"></i>Need help?</a></div>
-              <div className="dropdown-item"><a><i className="fas fa-sign-out-alt"></i>Sign out</a></div>
-            </div>
-          </div>
-          <div className={showNotification ? "block-profile " : "block-profile dp-none"}>
-            <div className="block-container">
-              {notificationElm}
-            </div>
-          </div>
+            <span className={showMultiTask ? "rectangle " : "rectangle dp-none"}></span>
+          </i>
         </div>
-      </nav> : null
-      }
-    </>
+      </div>
+      <div className={
+
+        showMultiTask ? "dropdown-profile " : "dropdown-profile dp-none"}
+        ref={refTasks}
+      >
+        <div className="block-container">
+
+          <div className="dropdown-item"><a><i class="far fa-user-circle"></i>Profile</a></div>
+          <div className="dropdown-item"><a><i class="fas fa-cog"></i>Setting</a></div>
+          <div className="dropdown-item"><a><i class="far fa-envelope"></i>Inbox</a></div>
+          <div className="dropdown-item"><a><i class="far fa-question-circle"></i>Need help?</a></div>
+          <div className="dropdown-item"><a><i class="fas fa-sign-out-alt"></i>Sign out</a></div>
+        </div>
+      </div>
+      <div
+        className={showNotification ? "block-profile " : "block-profile dp-none"}
+        ref={refNotis}
+      >
+        <div className="block-container">
+          {notificationElm}
+        </div>
+      </div>
+    </nav >
   )
 }
