8175994316f31c85cb5f941376b54fc70d442244
thaomike <51148093+thaomike@users.noreply.github.com>
2020-09-02 21:11:30 +0700

    fix ldp

--- src/view/LadingPage/components/SlideSponsor.js
+++ src/view/LadingPage/components/SlideSponsor.js
@@ -12,13 +12,13 @@ import huong from '../assets/huong.jpg'
 export default function SlideSponsor() {
     const [mem, setMem] = useState([
         {
-            image: 'https://scontent.fhan3-3.fna.fbcdn.net/v/t1.0-9/s960x960/83794155_2551944795024669_2545637553109729280_o.jpg?_nc_cat=106&_nc_sid=174925&_nc_ohc=53sKtee5ck8AX-Xj6vy&_nc_ht=scontent.fhan3-3.fna&_nc_tp=7&oh=66cadfc19efac5623ca9f732e8401024&oe=5EC3BA88',
+            image: linh,
             name: 'Nguyễn Văn Học',
             khoa: 'D18',
             rank: 'gold'
         },
         {
-            image: 'https://scontent.fhan4-1.fna.fbcdn.net/v/t1.0-9/s960x960/79406775_2390572367922230_7389855695102803968_o.jpg?_nc_cat=104&_nc_sid=174925&_nc_ohc=4jwSTXuc4bMAX8kkd3w&_nc_ht=scontent.fhan4-1.fna&_nc_tp=7&oh=ab962b3fdec05ee12d0bed8f31393a59&oe=5EC2F2BB',
+            image: huong,
             name: 'Trần Minh Quang',
             khoa: 'D18',
             rank: 'gold'
@@ -36,13 +36,13 @@ export default function SlideSponsor() {
             rank: 'silver'
         },
         {
-            image: 'https://scontent.fhan3-2.fna.fbcdn.net/v/t31.0-8/p960x960/17359037_1985626588331570_566222989922575259_o.jpg?_nc_cat=111&_nc_sid=174925&_nc_ohc=6f5PqvqLyOcAX9YXZfs&_nc_ht=scontent.fhan3-2.fna&_nc_tp=6&oh=16780da9a162d16e215fad4760fa2de7&oe=5EC36320',
+            image: huong,
             name: 'Nguyễn Tiến Hải',
             khoa: 'D17',
             rank: 'diamond'
         },
         {
-            image: 'https://scontent.fhan3-1.fna.fbcdn.net/v/t1.0-9/s960x960/81728579_1419518378201108_7874011037505159168_o.jpg?_nc_cat=102&_nc_sid=174925&_nc_ohc=oHWFB_LNsaQAX-_zTJM&_nc_ht=scontent.fhan3-1.fna&_nc_tp=7&oh=1b213bb14fccf5549035716efe0e6ab9&oe=5EC5636A',
+            image: linh,
             name: 'Đô Đô',
             khoa: 'D17',
             rank: 'diamond'
