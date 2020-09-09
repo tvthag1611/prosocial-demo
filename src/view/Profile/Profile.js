9025945a5c63bff076a1e01f5e89b875514f3b1e
vhg_vip <viethuong24092000@gmail.com>
2020-09-09 21:56:59 +0700

    update profile

--- src/view/Profile/Profile.js
+++ src/view/Profile/Profile.js
@@ -4,20 +4,87 @@ import { useSelector } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
 
 export default function Profile() {
-  const {user} = useSelector(state => state.homeReducer)
-  
+  const { user } = useSelector(state => state.homeReducer);
+  const [showMenu, setShowMenu] = useState();
+
+  const setMenu = (menu) => {
+    debugger;
+    setShowMenu(menu);
+    console.log(showMenu)
+  }
+
   return (
-    <div className ="profile-page">
+    <div className="profile-page">
       <div className="header">
         <div className="cover-box">
-          <img className="cover-photo" 
-            src={user.cover}/>
+          <img className="cover-photo"
+            src={user.cover} />
         </div>
         <div className="avatar-box">
-          <Link to={`/profile/${user.id}/image`}><img src={user.avatar} /></Link>
+          <Link to={`/profile/${user.id}/image`}><img className="avatar" src={user.avatar} /></Link>
           <h3 className="name"> {user.displayName} </h3>
         </div>
       </div>
+
+      <div className="body_container">
+        <div className="menu-bar row">
+          <ul className="nav-list">
+            <li className='nav-item nav-active'>
+              <a className='nav-link '>
+                {showMenu === 1
+                  ? 'Dòng thời gian'
+                  : showMenu === 2
+                    ? 'Giới thiệu'
+                    : showMenu === 3
+                      ? 'Ảnh'
+                      : 'Nhiệu vụ hàng tháng'}
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a
+                onClick={() => {
+                  debugger;
+                  setMenu(1)
+                }}
+                className={showMenu === 1 ? 'nav-link active' : 'nav-link'}
+              >
+                Dòng thời gian
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a
+                onClick={() => setMenu(2)}
+                className={showMenu === 2 ? 'nav-link active' : 'nav-link'}
+              >
+                Dòng thời gian
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a
+                onClick={() => setMenu(3)}
+                className={showMenu === 3 ? 'nav-link active' : 'nav-link'}
+              >
+                Dòng thời gian
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a
+                onClick={() => setMenu(4)}
+                className={showMenu === 4 ? 'nav-link active' : 'nav-link'}
+              >
+                Dòng thời gian
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+
     </div>
+
   )
 }
